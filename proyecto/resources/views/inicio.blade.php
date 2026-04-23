c1761bf981953064ed1d6d3f853a66bc4c2460be
atorvel2911 <atorvel2911@g.educaand.es>
2026-04-23 10:32:38 +0200

    Limpieza: eliminar alertas duplicadas en inicio.blade.php

--- proyecto/resources/views/inicio.blade.php
+++ proyecto/resources/views/inicio.blade.php
@@ -8,23 +8,7 @@
     <span class="badge bg-primary fs-6">
         {{ Auth::user()->nombre }} ({{ ucfirst(Auth::user()->tipo) }})
     </span>
-
-
 </div>
-
-@if(session('success'))
-    <div class="alert alert-success alert-dismissible fade show">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-    </div>
-@endif
-
-@if(session('error'))
-    <div class="alert alert-danger alert-dismissible fade show">
-        {{ session('error') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-    </div>
-@endif
 
 <div class="row">
     <!-- INCIDENCIAS (Visible para Admin y Operario) -->
